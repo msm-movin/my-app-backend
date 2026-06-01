2e550b43dcde5f91293a5617193b4e4af030dbae
msm-movin <mohdsalman77776@gmail.com>
2026-06-01 12:32:29 +0530

    mycrud cross url fix

--- src/main/java/org/setup/mycrud/controller/controller.java
+++ src/main/java/org/setup/mycrud/controller/controller.java
@@ -13,7 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173", "https://mscrud.netlify.app"})
 public class controller {
     @Autowired
     private UserServiceImp userServiceImp;
